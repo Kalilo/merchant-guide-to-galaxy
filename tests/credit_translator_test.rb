ea5d1602b1f8c76dcc41acd23c3a49d34c1ce0e9
Kalilo Hansmann <kalilohansmann@gmail.com>
2020-07-31 15:31:01 +0200

    Added test

--- tests/credit_translator_test.rb
+++ tests/credit_translator_test.rb
@@ -4,14 +4,28 @@ require 'pry'
 
 class CreditTranslatorTest < Minitest::Test
   def test_additions
+    ct = setup
+
+    assert ct.aliases['glob'] == 'I'
+    assert ct.definitions['Silver'] == 34
+    assert ct.definitions['Gold'] == 50
+  end
+
+  def test_questions
+    ct = setup
+
+    assert ct.answer_question('how much is Silver') == 'Silver is 34'
+  end
+
+  private
+
+  def setup
     ct = CreditTranslator.new
 
     ct.add_alias 'glob is I'
     ct.add_definition 'Silver is 34 Credits'
     ct.add_definition 'glob glob Gold is 100 Credits'
 
-    assert ct.aliases['glob'] == 'I'
-    assert ct.definitions['Silver'] == 34
-    assert ct.definitions['Gold'] == 50
+    ct
   end
 end
